f4a71640e2af552a7ddd68dafcccff3434c8ffef
fransisco <fransisco@tsmid.com>
2020-10-24 00:00:50 +0700

    add percobaan

--- app/src/main/java/com/siscofran/tabchips/MainActivity.kt
+++ app/src/main/java/com/siscofran/tabchips/MainActivity.kt
@@ -3,6 +3,7 @@ package com.siscofran.tabchips
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.TypedValue
 import android.view.ViewGroup
 import android.widget.LinearLayout
@@ -23,12 +24,26 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = pagerAdapter
         tabLayout.setupWithViewPager(viewPager)
 
+        // request dari mas firdaus
         val tabs = tabLayout.getChildAt(0) as ViewGroup
-        for (i in 0 until tabs.childCount - 1) {
+        val tes = tabs.getChildAt(0)
+        val layoutParams = tes.layoutParams as LinearLayout.LayoutParams
+        layoutParams.marginStart = dpToPx(resources, resources.getDimension(R.dimen.dimen_30dp)).toInt()
+        tes.layoutParams = layoutParams
+        tabLayout.requestLayout()
+        Log.i("TAG", "onCreate: ${tabs.childCount}")
+
+        val tes2 = tabs.getChildAt(tabs.childCount -1)
+        val layoutParams2 = tes2.layoutParams as LinearLayout.LayoutParams
+        layoutParams2.marginEnd = dpToPx(resources, resources.getDimension(R.dimen.dimen_30dp)).toInt()
+        tes2.layoutParams = layoutParams2
+        tabLayout.requestLayout()
+
+        for (i in 0 until tabs.childCount -1) {
             val tab = tabs.getChildAt(i)
             val layoutParams = tab.layoutParams as LinearLayout.LayoutParams
             layoutParams.marginEnd = dpToPx(resources, 8f).toInt()
-            layoutParams.marginStart = dpToPx(resources, 8f).toInt()
+//            layoutParams.marginStart = dpToPx(resources, 8f).toInt()
             tab.layoutParams = layoutParams
             tabLayout.requestLayout()
         }
